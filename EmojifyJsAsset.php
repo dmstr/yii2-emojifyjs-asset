a70bc55da5100908330b289d1960d015218386ae
Tobias Munk <schmunk@usrbin.de>
2016-09-22 10:52:09 +0200

    added asset bundle

---
+++ EmojifyJsAsset.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace dmstr\web;
+
+/**
+ * This is just an example.
+ */
+class EmojifyJsAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/emojify.js/dist';
+
+    public $css = [
+        'css/emojify.css'
+    ];
+
+    
+}
